9293f590af15a119bd2e526db6b18a326f18e84e
Rishi Saxena <rishi.saxena.gwl@gmail.com>
2020-07-25 23:51:40 +0530

    Remodeling of data

---
+++ src/app/models/hospitaldatamodel.ts
@@ -0,0 +1,13 @@
+export interface Hospital {
+    serial: String
+    ward: String
+    service: String
+    hospital: String
+    address: String
+    pincode: String
+    latlong: String
+    beds: String
+    name: String
+    contact: String
+    email: String
+}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,6 +3,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { ProviderService } from 'src/app/provider/provider.service';
 import { AppGlobals } from 'src/app/globals/app.global';
 import { Hospitals, Feed, Entry, GsCell } from 'src/app/models/hospitalmodel';
+import { Hospital } from 'src/app/models/hospitaldatamodel';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ import { Hospitals, Feed, Entry, GsCell } from 'src/app/models/hospitalmodel';
 })
 export class HomePage {
 
-  dataArray: String[][] = [];
+  dataArray: Hospital[] = [];
 
   constructor(public restProvider: ProviderService, private global: AppGlobals) {}
 
@@ -22,29 +23,30 @@ export class HomePage {
         (data: Hospitals) => {
           let entryArr: Entry[] = data.feed.entry; 
           let currentRow: number = 1; 
-          let titleData: String[] = [];
+          let titleData = [];
           for (var val of entryArr) {
             if(Number(val.gs$cell.row) == currentRow) {
-              titleData.push(val.gs$cell.inputValue);
+              titleData.push([val.title.$t, val.gs$cell.inputValue]);
             } else {
-              this.dataArray.push(titleData)
+              this.dataArray.push(this.getHospital(titleData))
               currentRow += 1
               break;
             }
           }
-          let rowData: String[] = []
+          let rowData = []
           for (var val of entryArr) {
             if(Number(val.gs$cell.row) > 1 && currentRow == Number(val.gs$cell.row)) {
-              rowData.push(val.gs$cell.inputValue);
+              rowData.push([val.title.$t, val.gs$cell.inputValue]);
             } else {
               if(Number(val.gs$cell.row) > 1) {
-                this.dataArray.push(rowData)
+                this.dataArray.push(this.getHospital(rowData))
                 rowData = []
-                rowData.push(val.gs$cell.inputValue);
+                rowData.push([val.title.$t, val.gs$cell.inputValue]);
                 currentRow += 1
               }
             }
           }
+          //console.log(this.dataArray)
         },
         (err: HttpErrorResponse) => {
           if (err.error instanceof ErrorEvent) {
@@ -54,5 +56,50 @@ export class HomePage {
           }
         }
       );
+   }
+
+   getHospital(objArr: String[]): Hospital {
+    var hospitalObj: Hospital = { serial: "", ward: "", service: "", hospital: "", address: "", pincode: "", latlong: "", beds: "", name: "", contact: "", email: ""}
+    for (var val of objArr) {
+      let key = val[0].charAt(0);
+      switch (key) {
+        case "A":
+          hospitalObj.serial = val[1]
+          break;
+        case "B":
+          hospitalObj.ward = val[1]
+          break;
+        case "C":
+          hospitalObj.service = val[1]
+          break;
+        case "D":
+          hospitalObj.hospital = val[1]
+          break;
+        case "E":
+          hospitalObj.address = val[1]
+          break;
+        case "F":
+          hospitalObj.pincode = val[1]
+          break;
+        case "G":
+          hospitalObj.latlong = val[1]
+          break;
+        case "H":
+          hospitalObj.beds = val[1]
+          break;
+        case "I":
+          hospitalObj.name = val[1]
+          break;
+        case "J":
+          hospitalObj.contact = val[1]
+          break;
+        case "K":
+          hospitalObj.email = val[1]
+          break;
+        default:
+          break;
+      }
+    }
+    return hospitalObj
    }
 }
